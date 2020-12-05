c30050f23679a410bba6717b8a45363a57609802
Daniel Travieso <daniel@dgtravieso.com>
2020-12-05 00:00:16 -0300

    finish part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -46,20 +46,136 @@ According to the above rules, your improved system would report 2 valid passport
 
 Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
 
+--- Part Two ---
+
+The line is moving more quickly now, but you overhear airport security talking about how passports with invalid data are getting through. Better add some data validation, quick!
+
+You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
+
+    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    cid (Country ID) - ignored, missing or not.
+
+Your job is to count the passports where all required fields are both present and valid according to the above rules. Here are some example values:
+
+byr valid:   2002
+byr invalid: 2003
+
+hgt valid:   60in
+hgt valid:   190cm
+hgt invalid: 190in
+hgt invalid: 190
+
+hcl valid:   #123abc
+hcl invalid: #123abz
+hcl invalid: 123abc
+
+ecl valid:   brn
+ecl invalid: wat
+
+pid valid:   000000001
+pid invalid: 0123456789
+
+Here are some invalid passports:
+
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+Here are some valid passports:
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+
+Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional. In your batch file, how many passports are valid?
+
 */
 use std::io;
 use std::io::prelude::*;
 use regex::Regex;
 use std::collections::HashMap;
 
+fn check(key: &str, value: &str) -> bool {
+    //println!("{} {}", key, value);
+    match key {
+        "byr_" => {
+            let year: i16 = value.parse().unwrap_or(0);
+            //println!(" {}", year);
+            return year >= 1920 && year <= 2002;
+        },
+        "iyr_" => {
+            let year: i16 = value.parse().unwrap_or(0);
+            //println!(" {}", year);
+            return year >= 2010 && year <= 2020;
+        },
+        "eyr_" => {
+            let year: i16 = value.parse().unwrap_or(0);
+            //println!(" {}", year);
+            return year >= 2020 && year <= 2030;
+        },
+        "hgt_cm" => {
+            let len = value.len();
+            let hgt: i16 = value[..len-2].parse().unwrap_or(0);
+            //println!(" {}", hgt);
+            return hgt >= 150 && hgt <= 193;
+        },
+        "hgt_in" => {
+            let len = value.len();
+            let hgt: i16 = value[..len-2].parse().unwrap_or(0);
+            //println!(" {}", hgt);
+            return hgt >= 59 && hgt <= 76;
+        }
+        _ => return true,
+    }
+}
 fn main() {
     let stdin = io::stdin();
     let mut buffer = String::new();
     let len = stdin.lock().read_to_string(&mut buffer).unwrap();
     //println!("{}", len);
     let re = Regex::new(r"((?P<k>\w{3}):(?P<v>\S+))+").unwrap();
+    let mut validate = HashMap::new();
+    validate.insert("byr".to_string(), Regex::new(r"^\d{4}$").unwrap());
+    validate.insert("iyr".to_string(), Regex::new(r"^\d{4}$").unwrap());
+    validate.insert("eyr".to_string(), Regex::new(r"^\d{4}$").unwrap());
+    validate.insert("hgt".to_string(), Regex::new(r"^\d+(cm|in)$").unwrap());
+    validate.insert("hcl".to_string(), Regex::new(r"^#[0-9a-f]{6}$").unwrap());
+    validate.insert("ecl".to_string(), Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap());
+    validate.insert("pid".to_string(), Regex::new(r"^\d{9}$").unwrap());
     let mut count = 0;
+    let mut rejected1 = 0;
+    let mut rejected2 = 0;
+    let mut rejected3 = 0;
+    let mut total = 0;
     for data in buffer.split("\n\n") {
+        total += 1;
         //println!("\n{:?}", data);
         let mut set = HashMap::new();
         for cap in re.captures_iter(data) {
@@ -68,19 +184,45 @@ fn main() {
         }
         let mut ok = true;
         for k in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].iter() {
-            //print!(" {}", k);
             if ! set.contains_key(&k.to_string()) {
+                rejected1 += 1;
                 ok = false;
-                //print!(" F");
-            } /*else {
-                print!(" T |");
-            }*/
+                break;
+            } else {
+                let re = validate.get(&k.to_string()).unwrap();
+                let v = set.get(&k.to_string()).unwrap();
+                let cap = re.captures(v);
+                if cap.is_none() {
+                    if ok != false {
+                        rejected2 += 1;
+                    }
+                    ok = false;
+                    break;
+                } else {
+                    let cap = cap.unwrap();
+                    let group = cap.get(1).map_or("", |m| m.as_str());
+                    if !check([k, group].join("_").as_str(), v) {
+                        if ok != false {
+                            rejected3 += 1;
+                        }
+                        ok = false;
+                        break;
+                    }
+                }
+            }
         }
-        //println!("{:?} {}", set, ok);
+        
         if ok {
             count += 1;
+            /*
+            for k in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].iter() {
+                print!("{} {}\t", k, v = set.get(&k.to_string()).unwrap());
+            }
+            println!();*/
+            //println!("{:?} {}", set, ok);
         }
     }
-    println!("{}", count);
+    println!("{} {} = {} + {} + {} + {}", len, total, count, rejected1, rejected2, rejected3);
+    assert_eq!(total, count + rejected1 + rejected2 + rejected3);
 
 }
