4cf90b52aeb4b6c79513f4c11c44b860a2e265b6
Daniel Travieso <daniel@dgtravieso.com>
2020-12-10 23:37:35 -0300

    finished part 2, took a long time to realize how it should be done

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 fn main() {
     let stdin = io::stdin();
     let mut adapters = Vec::new();
+
     adapters.push(0);
     for line in stdin.lock().lines() {
         let adapter = line.unwrap().parse::<i32>().unwrap();
@@ -13,16 +14,47 @@ fn main() {
     adapters.push(adapters[adapters.len()-1] + 3);
     let mut one = 0;
     let mut three = 0;
-    println!("{:?}", adapters);
+    //println!("{:?}", adapters);
 
     for i in 1..adapters.len() {
         let dif = adapters[i] - adapters[i-1];
-        if dif == 1 {
-            one += 1;
-        } else if dif == 3 {
-            three += 1;
+        match dif {
+            1 => one += 1,
+            2 => {},
+            3 => three += 1,
+            _ => panic!("shouldn't happen")
         }
     }
 
     println!("{} {} {}", one, three, one*three);
+
+    let mut dp = vec![0u128; adapters.len()];
+    dp[adapters.len()-1] = 1;
+
+    for i in (0..adapters.len()).rev() {
+        let i1: i32 = i as i32 - 1;
+        let i2: i32 = i as i32 - 2;
+        let i3: i32 = i as i32 - 3;
+        if i3 >= 0 {
+            let dif = adapters[i] - adapters[i3 as usize];
+            if dif <= 3 {
+                dp[i3 as usize] += dp[i];
+            }
+        }
+        if i2 >= 0 {
+            let dif = adapters[i] - adapters[i2 as usize];
+            if dif <= 3 {
+                dp[i2 as usize] += dp[i];
+            }
+        }
+        if i1 >= 0 {
+            let dif = adapters[i] - adapters[i1 as usize];
+            if dif <= 3 {
+                dp[i1 as usize] += dp[i];
+            }
+        }
+        //println!("{:?}", dp);
+    }
+    println!("{}", dp[0]);
+
 }
